a0e3638eb9c23411f984e425b08c6bf8f65a8828
Francisco <franciscoabreu1408@gmail.com>
2025-04-08 12:23:52 -0300

    Day one done

---
+++ Easy/stock.cpp
@@ -0,0 +1,23 @@
+// EASY - 121. Best Time to Buy and Sell Stock
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+    public:
+        int maxProfit(vector<int>& prices) {
+            int maxProfit;
+            int min;
+            min = prices[0];
+            maxProfit = 0;
+            for(int i = 0; i < prices.size(); i++) {
+                if(prices[i] < min) min = prices[i]; // When sees a value that is lower than the current min, saves it
+                // If the min is the current minimum, all the profits with the following values will be the best one.
+                if(maxProfit < prices[i] - min) maxProfit = prices[i] - min; // If none of the following profits are better, you keep the previous one saved
+            }
+           return maxProfit; 
+        }
+    };
---
+++ Easy/twoSum.cpp
@@ -0,0 +1,39 @@
+//1. EASY - twoSum
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> seen;
+        // IDEA: Iterates once in the array and takes what is left from each
+        // subtraction from the target value and the index value and saves in memory;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (seen.count(complement)) { // Check if any of the complements from previous subtractions fullfils the current one
+                return {seen[complement], i};
+            }
+            seen[nums[i]] = i; // Memory of what is left to complete with the idexes of each value
+        }
+        return {}; // no solution found
+    }
+};
+
+int main() {
+    Solution sol;
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> result = sol.twoSum(nums, target);
+
+    // Printing the result
+    for (int i : result) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
